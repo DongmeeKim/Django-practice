68156f7e6248de83832bd2daed47924308be795b
DongmeeKim <kdmee90@gmail.com>
2018-04-22 21:56:02 +0900

    프로젝트 연습(기초편)

--- election/models.py
+++ election/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Character(models.Model):
+    name = models.CharField(max_length = 15)
+    gender = models.CharField(max_length = 10)
+    dormitory = models.CharField(max_length = 15)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
--- election/views.py
+++ election/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Character
 # Create your views here.
 
-def index(response):
-    return HttpResponse("Hi everyone. Thank you for visiting.")
+def index(request):
+    characters = Character.objects.all()
+    str = "" # 마지막에 return 해줄 문자열
 
+    for character in characters :
+        str += "<p>{}는 {}기숙사에 배정 받았다.<br>".format(character.name, character.dormitory)
+        str += character.description + "<p>"
+
+    return HttpResponse(str)
