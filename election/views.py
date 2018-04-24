adc000b3bbac2e9c4ee244cb05661cf194bfde64
DongmeeKim <kdmee90@gmail.com>
2018-04-24 16:13:28 +0900

    프로젝트 연습(기초편)

--- election/views.py
+++ election/views.py
@@ -6,10 +6,5 @@ from .models import Character
 
 def index(request):
     characters = Character.objects.all()
-    str = "" # 마지막에 return 해줄 문자열
-
-    for character in characters :
-        str += "<p>{}는 {}기숙사에 배정 받았다.<br>".format(character.name, character.dormitory)
-        str += character.description + "<p>"
-
-    return HttpResponse(str)
+    context = {'characters' : characters}
+    return render(request, 'election/index.html', context)
