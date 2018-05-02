149b830958dada1f24a48f71e245b2482de5b03f
DongmeeKim <kdmee90@gmail.com>
2018-05-02 16:45:28 +0900

    프로젝트 연습(기초편)

--- election/urls.py
+++ election/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'elections'
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^dormitories/(?P<dormitory>.+)/$', views.dormitories),  
+    url(r'^$', views.index, name = 'home'),
+    url(r'^dormitories/(?P<dormitory>.+)/$', views.dormitories),
+    url(r'^dormitories/(?P<dormitory>.+)/results$', views.results), 
     url(r'^polls/(?P<poll_id>\d+)/$', views.polls),
 ]
--- election/views.py
+++ election/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Character, Poll, Choice
 import datetime
+
+from django.db.models import Sum
 # Create your views here.
 
 def index(request):
@@ -32,6 +34,29 @@ def polls(request, poll_id):
     except:
         choice = Choice(poll_id = poll_id, character_id = selection, votes =1)
         choice.save()
+    return HttpResponseRedirect("/dormitories/{}/results".format(poll.dormitory))
 
+def results(request, dormitory):
+    characters = Character.objects.filter( dormitory = dormitory )
+    polls = Poll.objects.filter( dormitory = dormitory )
+    poll_results = []
+    for poll in polls:
+        result = {}
+        result['start_date'] = poll.start_date
+        result['end_date'] = poll.end_date
+        total_votes = Choice.objects.filter( poll_id = poll.id ).aggregate(Sum('votes'))
+        result['total_votes'] = total_votes['votes__sum']
+        rates = []
+        for character in characters:
+            try:
+                choice = Choice.objects.get(poll = poll, character = character)
+                rates.append(
+                    round(choice.votes * 100 / result['total_votes'],1)
+                    )
+            except:
+                rates.append(0)
+        result['rates'] = rates
+        poll_results.append(result)
 
-    return HttpResponse("finish")
+    context = {'characters':characters, 'dormitory':dormitory, 'poll_results':poll_results}
+    return render(request, 'election/result.html', context)
